60b3cdf9da9ff2cacaf58889ce42bae0d3ca4a43
Tim Poerschke <info@coder-tim.de>
2018-05-19 21:41:40 +0200

    Added the HellwegPrintWriter
    Diese Unterklasse des PrintWriters wird die Runden und Chips schreiben
    (per print()).

---
+++ application/classes/io/HellwegPrintWriter.java
@@ -0,0 +1,34 @@
+package classes.io;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import classes.Chip;
+
+public class HellwegPrintWriter extends PrintWriter {
+
+	public HellwegPrintWriter(Writer writer) {
+		super(writer);
+	}
+
+	public HellwegPrintWriter(OutputStream os) {
+		super(os);
+	}
+	
+	public void print(Chip chip) {
+		println("<chip>");
+		
+		// Jedes Attribut schreiben.
+		println("<id>" + chip.getId() + "</id>");
+		println("<studentName>" + chip.getStudentName() + "</studentName>");
+		
+		// Runden schreiben
+		println("<rounds>");
+		// TODO: Hier die Runden schreiben.
+		println("</rounds>");
+		
+		println("</chip>");
+	}
+
+}
