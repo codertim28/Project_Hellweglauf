29e9b3b57310e60668f521bfb2ab5651d2515599
Tim Poerschke <info@coder-tim.de>
2018-11-20 21:11:16 +0100

    [FEATURE] Render studentname and lap count to the HTML document

--- application/classes/view/PrintView.java
+++ application/classes/view/PrintView.java
@@ -1,9 +1,13 @@
 package classes.view;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javax.print.PrintException;
@@ -11,6 +15,8 @@ import javax.print.PrintException;
 import classes.Data;
 import classes.model.Chip;
 import classes.model.Competition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableSet;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -25,6 +31,8 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.transform.Scale;
 import javafx.scene.web.WebView;
+import tp.dialog.StandardAlert;
+import tp.dialog.StandardMessageType;
 
 public class PrintView implements Initializable {
 	
@@ -53,6 +61,7 @@ public class PrintView implements Initializable {
 		}).findFirst().get(); // endlich das Element bekommen..
 		
 		Node node = this.previewWebView;
+		// Das WebView transformieren... bisschen unschön an dieser Stelle
         PageLayout pageLayout = printer.createPageLayout(Paper.A4, PageOrientation.PORTRAIT, Printer.MarginType.DEFAULT);
         double scaleX = pageLayout.getPrintableWidth() / node.getBoundsInParent().getWidth();
         double scaleY = pageLayout.getPrintableHeight() / node.getBoundsInParent().getHeight();
@@ -91,13 +100,50 @@ public class PrintView implements Initializable {
 			.filter(c -> c.getLapCount() > 0)
 			.forEach(c -> studentListView.getItems().add(c));
 		
-		// Die Preview setzen
+		// Wenn ein neues Item ausgewählt wurde: Die WebView rendern
+		studentListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Chip>() {
+		    @Override
+		    public void changed(ObservableValue<? extends Chip> observable, Chip oldValue, Chip newValue) {
+		        // Rendern mit dem ausgewählen Chip, welcher in newValue steht
+		        renderDocument(newValue);
+		    }
+		});
+		
+		// Die Preview setzen (den ersten Chip in der Liste rendern)
+		renderDocument(comp.getChipsController().getChips().get(0));
+		previewWebView.setZoom(0.38);
+
+	}
+	
+	// Rendert das HTML-Dokument mit einem entsprechendem Chip
+	private void renderDocument(Chip c) {
 		try {
-			previewWebView.getEngine().load(new File(Data.DIR + "/" + Data.BASIC_DIR + "/urkunde-vorlage.html").toURI().toURL().toString());
-			previewWebView.setZoom(0.38);
-		} catch(MalformedURLException e) {
+			// Die Vorlage auslesen
+			List<String> list = Files.readAllLines(new File(Data.DIR + "/" + Data.BASIC_DIR + "/urkunde-vorlage.html").toPath());
+		
+			// Zeile für Zeile in die Datei "letzterDruck.html" schreiben.
+			// Dabei werden die entsprechenden Templatevariablen ersetzt:
+			//   - <%name%>            Name des Schülers
+			//   - <%runden%>          Anzahl der gelaufenden Runden
+			//   - <%rundenliste%>     Übersicht über die gelaufenen Runden mit Zeitstempel
+			//   - <%wettkampfdetail%> (Optional) Infos über den Wettkampf 
+			PrintWriter pw = new PrintWriter(new FileWriter(Data.DIR + "/" + Data.BASIC_DIR + "/letzterDruck.html"));
+			
+			// Hier darf nicht gefiltert werden, da sonst HTML und CSS verloren geht
+			list.stream().map(line -> { 
+				return line = line.replaceAll("<%name%>", c.getStudentName())
+						.replaceAll("<%runden%>", "" + c.getLapCount());
+			}).forEach(pw::println);
+			
+			pw.close();
+			
+			// Anzeigen
+			previewWebView.getEngine().load(new File(Data.DIR + "/" + Data.BASIC_DIR + "/letzterDruck.html").toURI().toURL().toString());
+		
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			new StandardAlert(StandardMessageType.ERROR);
 		}
 	}
 
